d997eb8e79675e87b1286b85bbd0853fb86143cc
Richard Morgan <mba.rick@gmail.com>
2017-03-11 10:41:27 -0600

    refactor: seperated ModelView
    moved functions into ModelView and implemented knockout bindings

--- js/app.js
+++ js/app.js
@@ -5,20 +5,50 @@ var map;
 // blank array for all the markers
 var markers = [];
 
-function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 32.010394, lng: -102.107687},
-        zoom: 13
-    });
-
-    var locations = [
+var locations = [
         {title: 'Ricks House', location: {lat: 32.010855, lng: -102.107781}},
         {title: 'Fasken Park', location: {lat: 32.012297, lng: -102.106606}},
         {title: 'Connies House', location: {lat: 32.01101, lng: -102.107148}},
         {title: 'Lauras House', location: {lat: 32.010905, lng: -102.107486}},
         {title: 'Kims House', location: {lat: 32.010773, lng: -102.108157}},
     ];
+
+
+
+// VIEWMODEL
+
+      // This function populates the infowindow when the marker is clicked. We'll only allow
+      // one infowindow which will open at the marker that is clicked, and populate based
+      // on that markers position.
+      function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.open(map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick',function(){
+            infowindow.setMarker = null;
+          });
+        }
+      }
+
+
+
+var AppViewModel = function(){
+    var self = this;
+    this.ricksPlaces = ko.observableArray(locations);
+
+};
+
+AppViewModel.prototype.initMap = function(){
+    // Constructor creates a new map - only center and zoom are required.
+    var self = this;
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 32.010394, lng: -102.107687},
+        zoom: 13
+    });
 
     var largeInfowindow = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
@@ -40,6 +70,8 @@ function initMap() {
         markers.push(marker);
         // Create an onclick event to open an infowindow at each marker.
 
+        self.ricksPlaces()[i].marker=marker;
+
         marker.addListener('click', function() {
             populateInfoWindow(this, largeInfowindow);
         });
@@ -48,29 +80,15 @@ function initMap() {
         map.fitBounds(bounds);
     }
 
-        document.getElementById('show-ricksHood').addEventListener('click', showRicksHood);
-        document.getElementById('hide-ricksHood').addEventListener('click', hideRicksHood);
+        // document.getElementById('show-ricksHood').addEventListener('click', showRicksHood);
+        // document.getElementById('hide-ricksHood').addEventListener('click', hideRicksHood);
 
 }
-      // This function populates the infowindow when the marker is clicked. We'll only allow
-      // one infowindow which will open at the marker that is clicked, and populate based
-      // on that markers position.
-      function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
-          infowindow.open(map, marker);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick',function(){
-            infowindow.setMarker = null;
-          });
-        }
-      }
+
+
+var appViewModel = new AppViewModel();
 
 
 
-
-
-// ko.applyBindings(new AppViewModel());
+ko.applyBindings(appViewModel);
 
